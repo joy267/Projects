e4807bc5863278655e333ac0a6e60d7a71078a8f
Mrityunjoy_Mandal <mrityunjoy@solivatech.com>
2022-11-23 20:42:36 +0530

    first commit

---
+++ long_responses.py
@@ -0,0 +1,7 @@
+import random
+
+R_Eating = "I don't like eating anything because I'm a bot obviously!"
+
+def unknown():
+    response = ["Sorry! I couldn't understand what are you say..."][random.randrange(1)]
+    return response
